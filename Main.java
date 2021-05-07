13fb7c35e3293ca6cb81f932a9226ba55f054a8f
mdeister <Mjdeister@fjuhsd.org>
2021-05-06 23:24:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		System.out.println(RecursionFunOne.countEvenDigits(453222));
+		System.out.println(RecursionFunThree.luckyThrees(453222));
 		
 		//add more test cases
 	}
